34f18cf680642ff4446a5270c1695943381dd77c
F. Sabre <fabien.sabre@etu.imt-lille-douai.fr>
2021-02-02 23:05:50 +0100

    Use the project root for the paths of the config file

--- denseedia/config.py
+++ denseedia/config.py
@@ -1,7 +1,8 @@
 import json
 import pathlib
 
-config_path = pathlib.Path(__file__).parent.with_name("config.json")
+ROOT_PATH = pathlib.Path(__file__).parent.parent
+config_path = ROOT_PATH.joinpath("config.json")
 
 with config_path.open("r") as config_file:
     CONFIG = json.load(config_file)
